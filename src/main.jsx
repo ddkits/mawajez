10988086600b751208405875c512943a713ce6af
SamAyoub <m22386@live.com>
2025-11-10 16:22:51 -0500

    v1.131

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 import { I18nProvider } from './i18n.jsx'
 import App from './App'
 import './styles.css'
@@ -9,13 +9,13 @@ loadTheme();
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter future={{ 
+    <HashRouter future={{ 
   v7_startTransition: true,
   v7_relativeSplatPath: true 
 }}>
       <I18nProvider>
         <App />
       </I18nProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
